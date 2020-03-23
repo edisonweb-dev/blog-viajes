68da6ed080b86d2faa029fe1d5a8b0bccd929902
Edison Perdomo <edisonperdomodev@gmail.com>
2020-03-22 21:49:59 -0400

    Se termino la pagina blog

---
+++ single.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+?>
+<section class="container contenido">
+    <div class="row">
+        <?php
+            while(have_posts()): the_post();
+              the_content();
+            endwhile;
+          ?>
+    </div>
+
+</section>
+
+<?php
+get_footer();
